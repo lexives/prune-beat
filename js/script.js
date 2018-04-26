db7409bdf20a3c1752ff85481a9f4aebcbc6e1cd
lexives <lexives96@gmail.com>
2018-04-26 13:08:28 -0500

    Made navbar links change the view on the main page

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    // ON LOAD
+    // =======
+    $("#page-body").load("views/home.html");
+    
+    
+    // NAVIGATION
+    // ==========
+    // Change to Home page
+    $(".navbar-brand").click(function(){
+        $("#page-body").load("views/home.html");
+    });
+    
+    // Change to About page
+    $("#about-nav").click(function(){
+        $("#page-body").load("views/about.html");
+    });
+    
+    // Change to Music page
+    $("#music-nav").click(function(){
+        $("#page-body").load("views/music.html");
+    });
+    
+    // Change to Videos page
+    $("#videos-nav").click(function(){
+        $("#page-body").load("views/videos.html");
+    });
+    
+    // Change to News & Events page
+    $("#news-nav").click(function(){
+        $("#page-body").load("views/news.html");
+    });
+    
+    // Change to Shop page
+    $("#shop-nav").click(function(){
+        $("#page-body").load("views/shop.html");
+    });
+    
+    // Change to Contact Us page
+    $("#contact-nav").click(function(){
+        $("#page-body").load("views/contact.html");
+    });
+});
